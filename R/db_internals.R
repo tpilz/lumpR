7929e3ac95f2606f87a1b4b8d93f7e0c15b8757f
Till Francke <francke@uni-potsdam.de>
2015-06-10 14:35:48 +0200

    BUGFIX: rocky fraction of TCs was not considered in normalization, fixed

--- R/db_internals.R
+++ R/db_internals.R
@@ -102,14 +102,19 @@ filter_small_areas <- function(con, table, thres, verbose) {
     print(paste0("-> Processing table '", table, "' ..."))
   
   dat_contains <- sqlFetch(con, table)
+  if (table=="r_tc_contains_svc") #for TCs also the rocky fraction needs to be considered
+  {
+    res=sqlQuery(con, "select pid as tc_id, -1 as svc_id, frac_rocky as fraction from terrain_components")
+    dat_contains = rbind(dat_contains, res)
+  }
 
   # sum of 'fraction' should be 1 for every higher level class (rounding error allowed)
   dat_contains_sum <- tapply(dat_contains$fraction, list(parent=dat_contains[[1]]), sum)
   if(any(dat_contains_sum > 1.05 | dat_contains_sum < 0.99))
-    stop(paste0("Before removal of tiny areas: sum of fractions per higher level unit not always equal to one. Check table '", table, "'!"))
+    stop(paste0("Before removal of tiny areas: sum of fractions per higher level unit not always equal to one. Check table '", table, ifelse(table=="r_tc_contains_svc", " and terrain_components (column frac_rocky)",""),"'!"))
   
   # remove datasets where fraction < area_thresh
-  rows_rm <- which(dat_contains$fraction < thres)
+  rows_rm <- which(dat_contains$fraction < thres && dat_contains[,2]!=-1) #find entities below threshold, omit special case "rocky fractions"
   
   # further processing if any fraction < area_thresh only
   if(!any(rows_rm)) {
@@ -119,7 +124,7 @@ filter_small_areas <- function(con, table, thres, verbose) {
     print(paste0("-> The following datasets will be removed from '", table, "':"))
     print(dat_contains[rows_rm,])
     
-    # keep datasets where LUs equal to more than 10% of the respective parent class' area would be removed
+    # keep datasets where entities of more than 10% of the respective parent class' area would be removed
     lu_rm_sum <- tapply(dat_contains$fraction[rows_rm], list(parent=dat_contains[[1]][rows_rm]), sum)
     if(any(lu_rm_sum > 0.1)) {
       keep_lu <- which(lu_rm_sum > 0.1)
@@ -146,7 +151,30 @@ filter_small_areas <- function(con, table, thres, verbose) {
     for (s in 1:nrow(dat_contains_rm))
       dat_contains_new$fraction[s] <- dat_contains_rm$fraction[s] / dat_contains_sum[paste0(dat_contains_rm[[1]][s])]
     
+    
     # write updated data into database
+    if (table=="r_tc_contains_svc") #for TCs also the rocky fraction needs to be considered
+    {
+      rocky_frac=        dat_contains_new[dat_contains_new$svc_id==-1,] #extract information on rocky fraction - this needs to go into another table
+      rocky_frac$svc_id = NULL #was just a marker for rocky fractions, not needed anymore
+      dat_contains_new = dat_contains_new[dat_contains_new$svc_id!=-1,] #keep only real SVCs, discard rocky fractions
+    
+      terrain_components <- sqlFetch(con, "terrain_components")
+      terrain_components = merge(terrain_components, rocky_frac, by.x="pid", by.y="tc_id")
+      if (!identical(terrain_components$frac_rocky, terrain_components$fraction))
+      tryCatch(
+      {
+        sqlQuery(con, paste0("delete from terrain_components"))
+        sqlSave(channel=con, tablename = "terrain_components", dat=terrain_components, verbose=verbose, 
+                append=TRUE , test = FALSE, nastring = NULL, fast = TRUE, rownames = FALSE)
+      }, error = function(e) {
+        odbcClose(con)
+        stop(paste0("An error occured when updating table terrain_components. ",
+                    "Error message of the writing function: ", e))
+      }
+            )
+    }
+    
     tryCatch(
     {
       sqlQuery(con, paste0("delete from ", table))
