10d336021a752bc6858283568dd2b55514f10b38
Tobias Pilz <tpilz@uni-potsdam.de>
2015-07-21 13:05:08 +0200

    Solved copyright issues
    Solved copyright issues regarding Fortran source files.

--- src/RainySeason.f
+++ src/RainySeason.f
@@ -1,3 +1,23 @@
+C LUMP/RainySeason.f
+C Copyright (C) 1999 Friedrich-Wilhelm Gerstengarbe, Peter C. Werner
+C Copyright (C) 2000-2004 Andreas Güntner
+C Copyright (C) 2015 Tobias Pilz
+C 
+C This program is free software: you can redistribute it and/or modify
+C it under the terms of the GNU General Public License as published by
+C the Free Software Foundation, either version 3 of the License, or
+C (at your option) any later version.
+C 
+C This program is distributed in the hope that it will be useful,
+C but WITHOUT ANY WARRANTY; without even the implied warranty of
+C MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+C GNU General Public License for more details.
+C 
+C You should have received a copy of the GNU General Public License
+C along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+
 C  Last change Feb. 2015 by Tobias Pilz: wrapping souroutine around main program for use in R
 C  Code needs updating !!! Currently old Fortran 77, implicit declarations, no comments, lots of GOTO etc.
 C
--- src/calc_seasonality.f90
+++ src/calc_seasonality.f90
@@ -1,3 +1,23 @@
+! LUMP/calc_seasonality.f90
+! Copyright (C) 2014 Till Francke
+! Copyright (C) 2015 Tobias Pilz
+! 
+! This program is free software: you can redistribute it and/or modify
+! it under the terms of the GNU General Public License as published by
+! the Free Software Foundation, either version 3 of the License, or
+! (at your option) any later version.
+! 
+! This program is distributed in the hope that it will be useful,
+! but WITHOUT ANY WARRANTY; without even the implied warranty of
+! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+! GNU General Public License for more details.
+! 
+! You should have received a copy of the GNU General Public License
+! along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+! Code derived from WASA model rev. 135 file Hillslope/hymo_h.f90
+
+
 MODULE helpFunction
 implicit none
 
