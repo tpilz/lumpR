79e4ec9979a549edff8295f2a1ad6ba7f8fab4c6
Tobias Pilz <tpilz@uni-potsdam.de>
2017-12-06 15:17:14 +0100

    ENH: db_echse_input(): Added WASA_svc sharedParamNum scale_ks_a, scale_ks_b, and cal_ks; see echse_engines commit 593aa81f7e650dcd6705cf3d5f9b4d4f64d91bde

--- R/db_echse_input.R
+++ R/db_echse_input.R
@@ -1110,7 +1110,9 @@ db_echse_input <- function(
                                     "# Vegetation and land-cover parameters",
                                     "ext", "res_b", "drag_coef", "rough_bare", "eddy_decay", "rss_a", "rss_b",
                                     "# Soil water parameters",
-                                    "scale_ks", "Phil_cal", "var1", "var2", "var3", "var4", "var5", "frac1", "frac2", "frac3", "frac4", "frac5",
+                                    "scale_ks", "scale_ks_a", "scale_ks_b", "Phil_cal", "var1", "var2", "var3", "var4", "var5", "frac1", "frac2", "frac3", "frac4", "frac5",
+                                    "# Calibration factors (influencing some of the other parameters by multiplication / summation)",
+                                    "cal_ks",
                                     "# Choices",
                                     "choice_et", "choice_rcs", "choice_roughLen", "choice_plantDispl", "choice_gloradmax",
                                     "choice_inf", "choice_perc", "choice_soilmod",
@@ -1118,7 +1120,8 @@ db_echse_input <- function(
                                     "na_val", "ode_accuracy", "ode_max_iter", "choice_odesolve"),
                         value=c("", 2, 10, 2, 0.34, -0.14, 1.35, -0.35, 0.25, 0.50, 0.2, 0.7,
                                 "", 0.5, 25, 0.07, 0.01, 2.5, 26, -1,
-                                "", 15, 0.4, 9999, 9999, 9999, 9999, 9999, 0, 0, 0, 0, 0,
+                                "", 15, 0, 0, 0.4, 9999, 9999, 9999, 9999, 9999, 0, 0, 0, 0, 0,
+                                "", 1,
                                 "", 13, 2, 2, 2, 1, 3, 2, 1,
                                 "", -9999., 1e-3, 1e5, 36))
 
