1642221e82479187ff8d141f7c481ccc2d9500ab
Tobias Pilz <tpilz@uni-potsdam.de>
2017-12-07 14:30:48 +0100

    ENH: db_echse_input: Added WASA_svc sharedParamNum 'choice_constraint'

--- R/db_echse_input.R
+++ R/db_echse_input.R
@@ -1117,13 +1117,13 @@ db_echse_input <- function(
                                     "choice_et", "choice_rcs", "choice_roughLen", "choice_plantDispl", "choice_gloradmax",
                                     "choice_inf", "choice_perc", "choice_soilmod",
                                     "# Other",
-                                    "na_val", "ode_accuracy", "ode_max_iter", "choice_odesolve"),
+                                    "na_val", "ode_accuracy", "ode_max_iter", "choice_constraint", "choice_odesolve"),
                         value=c("", 2, 10, 2, 0.34, -0.14, 1, 0, 0.25, 0.50, 0.2, 0.7,
                                 "", 0.5, 25, 0.07, 0.01, 2.5, 26, -1,
                                 "", 15, 0, 0, 0.4, 9999, 9999, 9999, 9999, 9999, 0, 0, 0, 0, 0,
                                 "", 1, 1, 1, 1, 1, 1, 0.65, 0, 1, 1,
                                 "", 13, 2, 2, 2, 1, 3, 2, 1,
-                                "", -9999., 1e-3, 1e5, 36))
+                                "", -9999., 1e-3, 1e5, 0, 36))
 
   sharedpar_svc <- "sharedParamNum_WASA_svc.dat"
   if(!file.exists(paste(proj_dir, proj_name, "data", "parameter", sharedpar_svc, sep="/")) | overwrite){
