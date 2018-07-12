bb8d70be5256a2de4953522cbd5011002daa241a
Tobias Pilz <tpilz@uni-potsdam.de>
2018-07-12 11:56:25 +0200

    ENH: GRASS 7: adapted reservoir_outlet() (not yet tested under Windows)

--- R/reservoir_outlet.R
+++ R/reservoir_outlet.R
@@ -1,5 +1,5 @@
 # lumpR/reservoir_outlet.R
-# Copyright (C) 2014,2015,2017 Tobias Pilz
+# Copyright (C) 2014,2015,2017,2018 Tobias Pilz
 # 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -40,6 +40,8 @@
 #' @param overwrite \code{logical}. Shall output of previous calls of this function be
 #'      deleted? If \code{FALSE} the function returns an error if output already exists.
 #'      Default: \code{FALSE}.
+#' @param silent \code{logical}. Shall the function be silent (also suppressing warnings
+#'      of internally used GRASS functions)? Default: \code{FALSE}.
 #'      
 #' @return \code{SpatialPoints} object containing outlet position for each reservoir
 #'      polygon and vector file \code{outlets_vect} exported to GRASS location.
@@ -67,11 +69,37 @@ reservoir_outlet <- function(
   
   ### PARAMETERS ###
   keep_temp = FALSE,
-  overwrite = FALSE
-  
+  overwrite = FALSE,
+  silent=F
 ) {
   
+  
+  if(!silent) message("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
+  if(!silent) message("% START reservoir_outlet()")
+  if(!silent) message("%")
+  if(!silent) message("% Initialise function...")
+  
+  
   tryCatch(gmeta(), error = function(e) stop("Cannot execute GRASS commands. Maybe you forgot to run initGRASS()?"))
+  
+  # CLEAN UP AND RUNTIME OPTIONS #  
+  cmd_out <- execGRASS("g.version", intern=TRUE)
+  if (cmd_out=="")
+    stop("Couldn't connect to GRASS-session. Try removing any open sinks by calling 'sink()' repeatedly. Or restart R.")
+  
+  # suppress annoying GRASS outputs 
+  tmp_file <- file(tempfile(), open="wt")
+  sink(tmp_file, type="output")
+  
+  
+  # also supress warnings in silent mode
+  if(silent){
+    tmp_file2 <- file(tempfile(), open="wt")
+    sink(tmp_file2, type="message")
+    oldw <- getOption("warn")
+    options(warn = -1)
+  }
+  
   
   tryCatch({
     
@@ -79,7 +107,7 @@ reservoir_outlet <- function(
     if (is.null(flowacc) & is.null(dem))
       stop("'flowacc' and 'dem' are NULL. One of it must be specified (non-NULL)!")
     # remove mask if any
-    x <- execGRASS("r.mask", flags=c("r"), intern = T)
+    tryCatch(suppressWarnings(execGRASS("r.mask", flags=c("r"))), error=function(e){})
     
     if(is.null(res_vct))
       stop("You have to specify res_vct, the name of the reservoir vector file in the GRASS location to be used as input!")
@@ -87,31 +115,59 @@ reservoir_outlet <- function(
     
     # remove output of previous function calls if overwrite=T
     if (overwrite) {
-      cmd_out <- execGRASS("g.remove", type="raster", pattern=paste("*_t", flowacc, sep=","), flags=c("f", "b"), intern=T)
-      cmd_out <- execGRASS("g.remove", type="vector", pattern=paste("*_t", outlets_vect, sep=","), flags=c("f", "b"), intern=T)
+      cmd_out <- execGRASS("g.remove", type="raster,vector", pattern=paste("*_t", outlets_vect, sep=","), flags=c("f", "b"), intern=T)
     } else {
       # remove temporary maps in any case
-      cmd_out <- execGRASS("g.remove", type="raster", pattern="*_t", flags=c("f", "b"), intern=T)
+      cmd_out <- execGRASS("g.remove", type="raster,vector", pattern="*_t", flags=c("f", "b"), intern=T)
     }
     
     
+    if(!silent) message("% OK")
+    if(!silent) message("%")
+    if(!silent) message("% Determine highest flow accumulation value for each reservoir...")
     
     # GRASS calculation of flow accumulation
     if (is.null(flowacc) & !is.null(dem)) {
       flowacc <- "accum_t"
-      x <- execGRASS("r.watershed", elevation=dem, accumulation=flowacc, intern=T)
+      x <- execGRASS("r.watershed", elevation=dem, accumulation=flowacc, flags="s", intern=T)
     }
     
+    # make sure flowacc is integer
+    x <- execGRASS("r.mapcalc", expression=paste0("accum_t = round(", flowacc, ")"), flags=c("overwrite"), intern=T)
+    
     # convert reservoir vector to raster
-    x <- execGRASS("v.to.rast", input=res_vct, type="area", output="res_rast_t", use="cat", intern=T)
+    x <- execGRASS("v.to.rast", input=res_vct, output="res_rast_t", use="cat", intern=T)
     
     # calculate accumulation for reservoir zones
-    x <- execGRASS("r.mapcalculator", amap=flowacc, bmap="res_rast_t", outfile="accum_res_t",
-              formula="if(B, A)", intern=T)
+    x <- execGRASS("r.mapcalc", expression=paste0("accum_res_t = if(res_rast_t,", flowacc, ")"), intern=T)
+    
     
     # crossproduct of accum_res_t and res_rast_t to have both cats as labels in one raster file
     x <- execGRASS("r.cross", input="res_rast_t,accum_res_t", output="cross_t", intern=T)
-    x <- execGRASS("r.null", map="cross_t", setnull="0")
+    
+    # NOTE: categories needed to fix r.cross bug
+    cat_res <- execGRASS("r.stats", input="res_rast_t", flags=c("n"), intern=T, ignore.stderr = T)
+    cat_acc <- execGRASS("r.stats", input="accum_res_t", flags=c("n"), intern=T, ignore.stderr = T)
+    
+    # check for and correct error in r.cross, see https://lists.osgeo.org/pipermail/grass-user/2018-February/077934.html
+    cmd_out <- execGRASS("r.stats", input="cross_t", flags=c("n"), intern=T, ignore.stderr = T)
+    if(any(as.numeric(cmd_out) == 0)) {
+      # save category labels
+      cat_labs <- execGRASS("r.category", map="cross_t", separator=":", intern=T)
+      cat_labs <- strsplit(cat_labs[-1], ":")
+      cat_labs <- lapply(cat_labs, function(x) c(as.numeric(x[1]) +1, x[2]))
+      # add +1 to categories (destroys labels)
+      cmd_out <- execGRASS("r.mapcalc", expression="cross_t=cross_t+1", flags=c("overwrite"), intern=T)
+      # get missing category label
+      cat_lab_miss <- c(1, paste(c("category"), c(cat_res[1], cat_acc[1]), sep=" ", collapse = "; "))
+      # merge to stored labels
+      cat_labs_mod <- sapply(c(list(cat_lab_miss), cat_labs), paste, collapse=":")
+      # write to grass raster
+      write.table(cat_labs_mod, paste(tempdir(), "recl_t.txt", sep="/"), sep="\t", quote=F, row.names = F, col.names = F)
+      cmd_out <- execGRASS("r.category", map="cross_t", separator=":", rules=paste(tempdir(), "recl_t.txt", sep="/"), intern = T)
+      file.remove(paste(tempdir(), "recl_t.txt", sep="/"))
+    }
+    
     
     # get statistics into R (accumulation with coordinates of pixels for every reservoir)
     cmd_out <- execGRASS("r.stats", input="cross_t", flags=c("n", "g", "l"), intern = T, ignore.stderr = T)
@@ -126,24 +182,53 @@ reservoir_outlet <- function(
     accum_max <- do.call(rbind,lapply(split(accums,accums$cat),function(chunk) chunk[which.max(chunk$accum),]))
     
     
+    if(!silent) message("% OK")
+    if(!silent) message("%")
+    if(!silent) message("% Compile output...")
+    
     # to SPDF
     coordinates(accum_max) <- c("x", "y")
     accum_max_spdf <- SpatialPointsDataFrame(coordinates(accum_max), data = data.frame(cat=accum_max@data$cat), proj4string = CRS(getLocationProj()))
     
     # load reservoirs from GRASS
-    res_polygon <- readVECT6(res_vct, ignore.stderr = T)
+    suppressWarnings(res_polygon <- readVECT(res_vct, ignore.stderr = T))
+    # WINDOWS PROBLEM: delete temporary file otherwise an error occurs when calling writeVECT or readVECT again with the same (or a similar) file name 
+    if(.Platform$OS.type == "windows") {
+      dir_del <- dirname(execGRASS("g.tempfile", pid=1, intern=TRUE, ignore.stderr=T))
+      files_del <- grep(substr(res_vct, 1, 8), dir(dir_del), value = T)
+      file.remove(paste(dir_del, files_del, sep="/"))
+    }
     
     # assign reservoir table to outlet locations table
     res_out <- sp::merge(accum_max_spdf, res_polygon@data, by="cat")
+    suppressWarnings(proj4string(res_out) <- CRS(getLocationProj()))
     
     # put into GRASS
-    writeVECT6(res_out, outlets_vect, v.in.ogr_flags = "overwrite", ignore.stderr = T)
+    suppressWarnings(writeVECT(res_out, outlets_vect, v.in.ogr_flags = c("overwrite", "o"), ignore.stderr = T))
+    # WINDOWS PROBLEM: delete temporary file otherwise an error occurs when calling writeVECT or readVECT again with the same (or a similar) file name 
+    if(.Platform$OS.type == "windows") {
+      dir_del <- dirname(execGRASS("g.tempfile", pid=1, intern=TRUE, ignore.stderr=T))
+      files_del <- grep(substr(outlets_vect, 1, 8), dir(dir_del), value = T)
+      file.remove(paste(dir_del, files_del, sep="/"))
+    }
     
     
     # delete temp
     if(keep_temp == FALSE)
-      x <- execGRASS("g.mremove", rast="*_t", vect="*_t", flags=c("f", "b"), intern=T)
+      x <- execGRASS("g.remove", type="raster,vector", pattern="*_t", flags=c("f", "b"), intern=T)
     
+    
+    if(!silent) message("% OK")
+    if(!silent) message("%")
+    if(!silent) message("% DONE!")
+    if(!silent) message("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
+    
+    # stop sinking
+    closeAllConnections()
+    
+    # restore original warning mode
+    if(silent)
+      options(warn = oldw)
     
     # return spatial object
     return(res_out)
@@ -152,10 +237,17 @@ reservoir_outlet <- function(
   # exception handling
   }, error = function(e) {
     
-    x <- execGRASS("r.mask", flags=c("r"), intern = T)
+    # stop sinking
+    closeAllConnections()
+    
+    # restore original warning mode
+    if(silent)
+      options(warn = oldw)
+    
+    x <-tryCatch(suppressWarnings(execGRASS("r.mask", flags=c("r"), intern = T)), error=function(e){})
     
     if(keep_temp == FALSE)
-      x <- execGRASS("g.mremove", rast=paste0("*_t,",flowacc), vect=paste0("*_t,", outlets_vect), flags=c("f", "b"), intern=T)
+      x <- execGRASS("g.remove", type="raster,vector", pattern=paste("*_t",outlets_vect, sep=","), flags=c("f", "b"), intern=T)
     
     stop(paste(e))  
   })
