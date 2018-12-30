925936b8856cb604689a7ebceb2d75539a14c9a3
Till Francke <francke@uni-potsdam.de>
2018-12-30 13:41:24 +0100

    ENH: clarified docu and table for strategic reservoirs

--- inst/database/update_db_v26.sql
+++ inst/database/update_db_v26.sql
@@ -1,6 +1,6 @@
 CREATE TABLE reservoirs_strategic (
 	pid INT AUTO_INCREMENT NOT NULL COMMENT 'foreign key to subbas',
-	res_id INT NOT NULL COMMENT 'foreign key to subbas',
+	res_id INT COMMENT 'external ID (optional)',
 	name NVARCHAR(20) COMMENT 'reservoir name (optional)',
 	minlevel double COMMENT 'Initial minimum level [m]',
 	maxlevel double COMMENT 'Maximum water level [m]',
