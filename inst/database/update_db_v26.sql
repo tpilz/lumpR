4ee9625dc75923254f49df7c1759fbf03606636b
Till Francke <francke@uni-potsdam.de>
2019-01-04 11:41:01 +0100

    ENH: computationally irrelevant: fixed wrong column description in SQL-table; increase version number

--- inst/database/update_db_v26.sql
+++ inst/database/update_db_v26.sql
@@ -17,7 +17,7 @@ CREATE TABLE reservoirs_strategic (
 	damb double COMMENT 'Parameters of the area-volume relationship (area=dama*Vol^damb) [-]',
 	q_outlet double COMMENT 'Maximum outflow discharge released through the bottom outlets [m^3/s]',
 	fvol_botm double COMMENT 'Fraction of storage capacity that indicates the minimum storage volume for sediment release through the bottom outlets [-]',
-	fvol_over double COMMENT 'Fraction of storage capacity that indicates the minimum storage volume for water release through the spillway [-]',
+	fvol_over double COMMENT 'Flag to simulate the retention of reservoir overflow during spillway operation [0 = without time delay; 1 = with time delay]',
 	damc double COMMENT 'Parameters of the spillway rating curve (Qout=damc*Hv^damd) [-]',
 	damd double COMMENT 'Parameters of the spillway rating curve (Qout=damc*Hv^damd) [-]',
 	elevbottom double COMMENT 'bottom outlet elevation [m]',
