13a80b802ea83f8905dad9f59ffcdb08b31f0352
TillF <francke@uni-potsdam.de>
2020-05-11 17:41:08 +0200

    Anne's fix for preserving attributes during snapping

--- R/internals.R
+++ R/internals.R
@@ -49,3 +49,72 @@ clean_temp_dir <- function(file_name) {
 molle_v <- function(alpha, k, A) k* (A/(alpha*k))^(alpha/(alpha-1)) 
 molle_a <- function(alpha, k, V) (V/k * (alpha * k)^(alpha/(alpha-1)))^((alpha-1)/alpha) #cas
 molle_h <- function(alpha, k, A) exp(log(A/alpha/k)/(alpha-1))
+
+
+
+
+# Addition for subbas.R (calc_subbas)
+# Modification in function snapPointsToLines
+# Fixes problem, that attributes of SpatialPointsDataFrame "drain_points"
+# are not passed when snapping drain_points (subbas.R, Line 351)
+# Source: https://stackoverflow.com/questions/60945115/snappointstolines-cant-keep-attributes-in-r
+
+snapPointsToLines1 <-  function (points, lines, maxDist = NA, withAttrs = TRUE, idField = NA) 
+{
+  if (rgeosStatus()) {
+    if (!requireNamespace("rgeos", quietly = TRUE)) 
+      stop("package rgeos required for snapPointsToLines")
+  }
+  else stop("rgeos not installed")
+  if (is(points, "SpatialPointsDataFrame")==FALSE && missing(withAttrs)) 
+    withAttrs = FALSE
+  if (is(points, "SpatialPointsDataFrame")==FALSE && withAttrs == TRUE) 
+    stop("A SpatialPointsDataFrame object is needed! Please set withAttrs as FALSE.")
+  d = rgeos::gDistance(points, lines, byid = TRUE)
+  if (!is.na(maxDist)) {
+    distToLine <- apply(d, 2, min, na.rm = TRUE)
+    validPoints <- distToLine <= maxDist
+    distToPoint <- apply(d, 1, min, na.rm = TRUE)
+    validLines <- distToPoint <= maxDist
+    points <- points[validPoints, ]
+    lines = lines[validLines, ]
+    d = d[validLines, validPoints, drop = FALSE]
+    distToLine <- distToLine[validPoints]
+    if (!any(validPoints)) {
+      if (is.na(idField)) {
+        idCol = character(0)
+      }
+      else {
+        idCol = lines@data[, idField][0]
+      }
+      newCols = data.frame(nearest_line_id = idCol, snap_dist = numeric(0))
+      if (withAttrs) 
+        df <- cbind(points@data, newCols)
+      else df <- newCols
+      res <- SpatialPointsDataFrame(points, data = df, 
+                                    proj4string = CRS(proj4string(points)), match.ID = FALSE)
+      return(res)
+    }
+  }
+  else {
+    distToLine = apply(d, 2, min, na.rm = TRUE)
+  }
+  nearest_line_index = apply(d, 2, which.min)
+  coordsLines = coordinates(lines)
+  coordsPoints = coordinates(points)
+  mNewCoords = vapply(1:length(points), function(x) nearestPointOnLine(coordsLines[[nearest_line_index[x]]][[1]], 
+                                                                       coordsPoints[x, ]), FUN.VALUE = c(0, 0))
+  if (!is.na(idField)) {
+    nearest_line_id = lines@data[, idField][nearest_line_index]
+  }
+  else {
+    nearest_line_id = sapply(slot(lines, "lines"), 
+                             function(i) slot(i, "ID"))[nearest_line_index]
+  }
+  if (withAttrs) 
+    df = cbind(points@data, data.frame(nearest_line_id, snap_dist = distToLine))
+  else df = data.frame(nearest_line_id, snap_dist = distToLine, 
+                       row.names = names(nearest_line_index))
+  SpatialPointsDataFrame(coords = t(mNewCoords), data = df, 
+                         proj4string = CRS(proj4string(points)))
+}
