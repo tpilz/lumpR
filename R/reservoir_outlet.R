9792af0f60606afa5678e19c0ef7854169a4c6a5
TillF <francke@uni-potsdam.de>
2025-12-05 14:24:01 +0100

    Handle existing column "area" in reservoir vector.

--- R/reservoir_outlet.R
+++ R/reservoir_outlet.R
@@ -116,7 +116,7 @@ reservoir_outlet <- function(
   if (!any(grepl(cmd_out, pattern="\\|area"))) #add area field in hectars, if not present
   {  
     x <- execGRASS("v.db.addcolumn", map=res_vct, columns="area double", intern=TRUE) 
-    x <- execGRASS("v.to.db", map=res_vct, option="area", columns="area", units="hectares", intern=TRUE) 
+    x <- execGRASS("v.to.db", map=res_vct, option="area", columns="area", units="hectares", intern=TRUE, flags="overwrite") 
   }
   
   
